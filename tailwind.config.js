a1402f6200c708b776823432c656c7639a6c7525
Alice Xiong <whoisrabbit@gmail.com>
2025-04-27 09:10:57 -0700

    Simplify: Remove custom font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-inter)'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } 
